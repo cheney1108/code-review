170838aec62f3472dc9afd4d914678f74f22d7f7
GitHub Action <action@github.com>
2024-04-23 02:12:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/cheney1108/codereview/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/cheney1108/codereview/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cheney1108.codereview.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/cheney1108/codereview/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cheney1108/codereview/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.cheney1108.codereview.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cheney1108.codereview.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
